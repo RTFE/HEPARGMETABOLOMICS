737be5d12e275c2b3920409dd728fe7c87e213c8
RTFE <93748093+RTFE@users.noreply.github.com>
2022-05-09 11:04:17 +0000

    regenerate html

--- include/ensenso/ensenso_grabber.h
+++ include/ensenso/ensenso_grabber.h
@@ -75,3 +75,74 @@ public:
     * The calibration routine currently supports two types of setups: either your camera is fixed with
     * respect to the robot origin, or your camera mounted on the robot hand and is moving with the robot.
     * @param[in] robot_poses A list of robot poses, 1 for each pattern acquired (in the same order)
+    * @param[in] camera_seed Initial guess of the camera pose. The pose must be given relative to the
+    * robot hand (for a moving camera setup), or relative to the robot origin (for the fixed camera setup).
+    * @param[in] pattern_seed Initial guess of the pattern pose. This pose must be given relative to the
+    * robot hand (for a fixed camera setup), or relative to the robot origin (for the moving camera setup).
+    * @param[in] setup Moving or Fixed, please refer to the Ensenso documentation
+    * @param[out] estimated_camera_pose The Transformation between this camera's left eye coordinates and
+    * the next linked system.
+    * @param[out] estimated_pattern_pose The estimated pattern pose. This pose is either relative to the
+    * robot hand (for a fixed camera setup), or relative to the robot origin (for the moving camera setup).
+    * @param[out] iterations Indicates the number of optimization iterations performed on the final solution
+    * until it had converged.
+    * @param[out] reprojection_error The reprojection error per pattern point over all collected patterns of
+    * the final solution.
+    * @return True if successful, false otherwise
+    * @warning This can take up to 120 seconds */
+    bool calibrateHandEye ( const std::vector<Eigen::Affine3d, Eigen::aligned_allocator<Eigen::Affine3d> > &robot_poses,
+                            const Eigen::Affine3d &camera_seed,
+                            const Eigen::Affine3d &pattern_seed,
+                            const std::string setup,
+                            Eigen::Affine3d &estimated_camera_pose,
+                            Eigen::Affine3d &estimated_pattern_pose,
+                            int &iterations,
+                            double &reprojection_error) const;
+
+    /** @brief Closes all Ensenso devices
+     * @return True if successful, false otherwise */
+    bool closeDevices ();
+
+    /** @brief Close TCP port program
+     * @return True if successful, false otherwise
+     * @warning If you do not close the TCP port the program might exit with the port still open, if it is the case
+     * use @code ps -ef @endcode and @code kill PID @endcode to kill the application and effectively close the port. */
+    bool closeTcpPort (void);
+
+    /** @brief Collects a calibration pattern
+     * @param[in] buffer Specifies whether the pattern should be added to the pattern buffer.
+     * @return the number of calibration patterns stored in the @code nxTree @endcode , -1 on error
+     * @warning A device must be opened and must not be running.*/
+    int collectPattern (const bool buffer=true) const;
+
+    /** @brief Decodes the pattern grid size and thickness
+     * @return the grid size in mm*/
+    double decodePattern () const;
+
+    /** @brief Clears the pattern buffers of monocular and stereo pattern observations.
+     * @return True if successful, false otherwise
+     * @note The counters PatternCount and MonocularPatternCount will be zero after clearing.*/
+    bool discardPatterns () const;
+
+    /** @brief Searches for available devices
+     * @returns The number of Ensenso devices connected */
+    int enumDevices () const;
+
+    /** @brief Estimate the calibration pattern pose
+     * @param[out] pose the calibration pattern pose
+     * @param[in] average Specifies if all pattern point coordinates in the buffer
+     * should be averaged to produce a more precise pose measurement. This will only
+     * produce a correct result if all patterns in the buffer originate from
+     * multiple images of the same pattern in the same pose.
+     * @return true if successful, false otherwise
+     * @warning A device must be opened and must not be running.
+     * @note At least one calibration pattern must have been collected before, use collectPattern() before */
+    bool estimatePatternPose (Eigen::Affine3d &pose, const bool average=false) const;
+
+    /** @brief Get class name
+     * @returns A string containing the class name */
+    std::string getName () const;
+
+    /** @brief Get meta information for a monocular camera.
+     * @param[in] cam A string containing the camera (Left or Right)
+     * @param[out] cam_info meta information for a camera.
