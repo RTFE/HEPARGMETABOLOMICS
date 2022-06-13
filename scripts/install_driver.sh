838d985253e16c7dd971d821305af8123dfbc514
RTFE <93748093+RTFE@users.noreply.github.com>
2022-06-13 21:03:15 +0000

    add stats

--- scripts/install_driver.sh
+++ scripts/install_driver.sh
@@ -20,4 +20,4 @@
 # Lots of logics in this file are specific to Docker. When used on a local host, you will need sudo, but won't need some tweeks.
 
 # Update the URL when newer versions available.
-URL_CODEMETER=https://download.en
+URL_CODEMETER=https://download.ensenso.com/s/ensensosdk/download?files=codemeter_6.40.240
