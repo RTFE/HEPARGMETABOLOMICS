1d8d96527a6cccae008bafb04f1f3e795242f8d0
RTFE <93748093+RTFE@users.noreply.github.com>
2022-06-18 20:28:23 +0000

    v2.4.3

--- scripts/snapshot_to_bag.py
+++ scripts/snapshot_to_bag.py
@@ -8,4 +8,7 @@ from rosbag.bag import Bag
 def callback_rgb(data, time, bag):
     print 'received PointCloud2!'
     data.header.stamp = time + rospy.Duration.from_sec(1505388514)   # shift time stamp to the year 2017 (for kalibr)
-    bag.write('/ensenso/depth/poin
+    bag.write('/ensenso/depth/points', data, t=data.header.stamp)
+
+def rgb_listener(bag):
+    counter 
