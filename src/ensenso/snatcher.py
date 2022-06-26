bf69bb9696334b871daafc29e20d588dabf8f165
RTFE <93748093+RTFE@users.noreply.github.com>
2022-06-26 14:43:55 +0000

    First Code

--- src/ensenso/snatcher.py
+++ src/ensenso/snatcher.py
@@ -28,4 +28,7 @@ class EnsensoDriverReconfigure():
     pass
 
   def get_configuration(self, timeout=None):
-    return self.dynclient.get_configuration(timeout=ti
+    return self.dynclient.get_configuration(timeout=timeout)
+
+  def update_configuration(self, config):
+    
