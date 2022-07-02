16186f06481c48fc69e33cc5e2f9ed8e66cdea1c
RTFE <93748093+RTFE@users.noreply.github.com>
2022-07-02 20:21:33 +0000

    Refactor imports

--- src/ensenso/snatcher.py
+++ src/ensenso/snatcher.py
@@ -63,4 +63,5 @@ class Snatcher(object):
   def __init__(self, use_cv_types=True):
     """
     Snatcher constructor. It subscribes to the following topics:
-      - Raw images: C{left/image_raw} and C{left/image_raw} of 
+      - Raw images: C{left/image_raw} and C{left/image_raw} of type C{sensor_msgs/Image}
+      - Rectified images: C{left/
