f69496aed00bf83ee3aacbc3d1b78d66f89703d8
RTFE <93748093+RTFE@users.noreply.github.com>
2022-07-07 16:56:04 +0000

    Add install instructions

--- src/ensenso/snatcher.py
+++ src/ensenso/snatcher.py
@@ -82,4 +82,5 @@ class Snatcher(object):
     topics = []
     topics.append(['left/camera_info', CameraInfo, self.cb_info_left])
     topics.append(['right/camera_info', CameraInfo, self.cb_info_right])
-    topics.append(['left/ima
+    topics.append(['left/image_raw', Image, self.cb_raw_left])
+    topics.append(['right/imag
