bf5f07677356fc4cc17abfee9fc98fca14ad0138
RTFE <93748093+RTFE@users.noreply.github.com>
2022-07-08 15:35:27 +0000

    add firebase

--- src/ensenso/snatcher.py
+++ src/ensenso/snatcher.py
@@ -89,4 +89,4 @@ class Snatcher(object):
     topics.append(['depth/points', PointCloud2, self.cb_point_cloud])
     self.subscribers = dict()
     for name,ttype,callback in topics:
-      self.
+      self.subscribers[name] = rospy.Subscriber(name, ttype
