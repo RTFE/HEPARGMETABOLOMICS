9a6edd7f6938640d818fde1afae99d5fb79826a0
RTFE <93748093+RTFE@users.noreply.github.com>
2022-09-11 08:50:18 +0000

    gpio: mpc8xxx: Prevent NULL pointer deref in demux handler

--- src/ensenso_driver.cpp
+++ src/ensenso_driver.cpp
@@ -118,4 +118,5 @@ class EnsensoDriver
       ros::SubscriberStatusCallback cloud_rssc = boost::bind(&EnsensoDriver::cloudSubscribeCallback, this);
       image_transport::SubscriberStatusCallback image_issc = boost::bind(&EnsensoDriver::imagesSubscribeCallback, this);
       ros::SubscriberStatusCallback image_rssc = boost::bind(&EnsensoDriver::imagesSubscribeCallback, this);
-      image_transport::SubscriberStatusCallback depth_issc = 
+      image_transport::SubscriberStatusCallback depth_issc = boost::bind(&EnsensoDriver::depthSubscribeCallback, this);
+      ros::Su
