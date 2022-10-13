661ea2a0d3ce84e74d476cf6c68fa218aab0b3d7
RTFE <93748093+RTFE@users.noreply.github.com>
2022-10-13 06:37:02 +0000

    Remove debugging message.

--- src/ensenso_driver.cpp
+++ src/ensenso_driver.cpp
@@ -248,4 +248,5 @@ class EnsensoDriver
       }
       ROS_DEBUG("---");
       ROS_DEBUG("Capture Parameters");
-      ROS_DEBUG_STREAM("AutoBlackLevel: "   << std::boolalpha << config.AutoBlackLevel
+      ROS_DEBUG_STREAM("AutoBlackLevel: "   << std::boolalpha << config.AutoBlackLevel);
+      ROS_DEBUG_STREAM("AutoExposure: "     << std:
