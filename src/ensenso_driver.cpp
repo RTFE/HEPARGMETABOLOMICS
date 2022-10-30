028d97c419b0579701853d86d2dae1c2bb44ce2e
RTFE <93748093+RTFE@users.noreply.github.com>
2022-10-30 13:17:57 +0000

    fix issue #1

--- src/ensenso_driver.cpp
+++ src/ensenso_driver.cpp
@@ -291,4 +291,5 @@ class EnsensoDriver
       ROS_DEBUG("Render Parameters");
       ROS_DEBUG_STREAM("SurfaceConnectivity: "   << std::boolalpha << config.SurfaceConnectivity);
       ROS_DEBUG_STREAM("NearPlane: "   << std::boolalpha << config.NearPlane);
-      ROS_DEBUG_STREAM("FarPlane: "   << std::boolalpha << conf
+      ROS_DEBUG_STREAM("FarPlane: "   << std::boolalpha << config.FarPlane);
+      ROS_DEBUG_STREAM("UseOpenGL: "   << std
