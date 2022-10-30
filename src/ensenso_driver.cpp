224cc3d1f32c3446bf0996a2d1d914797bb34bc6
RTFE <93748093+RTFE@users.noreply.github.com>
2022-10-30 18:02:27 +0000

    new line

--- src/ensenso_driver.cpp
+++ src/ensenso_driver.cpp
@@ -295,4 +295,4 @@ class EnsensoDriver
       ROS_DEBUG_STREAM("UseOpenGL: "   << std::boolalpha << config.UseOpenGL);
       ROS_DEBUG("Stream Parameters");
       ROS_DEBUG_STREAM("Cloud: "   << std::boolalpha << config.Cloud);
- 
+      ROS_DEBUG_STREAM("Images: "   << std::boolalpha << confi
