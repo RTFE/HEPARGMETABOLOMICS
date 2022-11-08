eaa4c06b589ef04f4d841f31dc8a595e7d04243c
RTFE <93748093+RTFE@users.noreply.github.com>
2022-11-08 14:34:18 +0000

    local commit

--- src/ensenso_driver.cpp
+++ src/ensenso_driver.cpp
@@ -316,4 +316,4 @@ class EnsensoDriver
         pattern_pose_pub_ = nh_.advertise<geometry_msgs::PoseStamped> ("pattern/pose", 1, false);
         calibrate_srv_ = nh_.advertiseService("calibrate_handeye", &EnsensoDriver::calibrateHandEyeCB, this);
         pattern_srv_ = nh_.advertiseService("estimate_pattern_pose", &EnsensoDriver::estimatePatternPoseCB, this);
-        collect_srv_ = nh_.advertiseSer
+        collect_srv_ = nh_.advertiseService("collect_pattern", &EnsensoDriver::collectPatternC
