ad25da1fcb583b232f640cb247bb3fca43707c61
RTFE <93748093+RTFE@users.noreply.github.com>
2022-11-22 14:28:29 +0000

    after

--- src/ensenso_driver.cpp
+++ src/ensenso_driver.cpp
@@ -355,4 +355,5 @@ class EnsensoDriver
       ensenso_ptr_->setMedianFilterRadius(config.MedianFilterRadius);
       ensenso_ptr_->setSpeckleComponentThreshold(config.SpeckleComponentThreshold);
       ensenso_ptr_->setSpeckleRegionSize(config.SpeckleRegionSize);
-      ensenso_ptr_->setFillBorderSpread(config.FillBorderSp
+      ensenso_ptr_->setFillBorderSpread(config.FillBorderSpread);
+      ensenso_ptr_->setFillRegionSize(config.FillRegionS
