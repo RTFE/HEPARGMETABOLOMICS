6952e52a61e242926ac56c005839fd79b5d3f87e
RTFE <93748093+RTFE@users.noreply.github.com>
2022-12-22 05:30:27 +0000

    chore(release): 1.1.1

--- src/ensenso_driver.cpp
+++ src/ensenso_driver.cpp
@@ -520,4 +520,5 @@ class EnsensoDriver
       if (l_raw_pub_.getNumSubscribers() > 0)
         l_raw_pub_.publish(*toImageMsg(rawimages->first, stamp, camera_frame_id_), linfo, stamp);
       if (r_raw_pub_.getNumSubscribers() > 0)
-        r_raw_pub_.publish(*toImageMsg(rawimages->sec
+        r_raw_pub_.publish(*toImageMsg(rawimages->second, stamp, camera_frame_id_), rinfo, stamp);
+      if (l_rectified
