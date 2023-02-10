f48ac7dc3dea3e1d2432884cb2785f7f16d86fd9
RTFE <93748093+RTFE@users.noreply.github.com>
2023-02-10 06:54:06 +0000

    Maven artifacts for Mental State Factory version \t\t\t\t\t\t2.0.2-SNAPSHOT

--- src/ensenso_grabber.cpp
+++ src/ensenso_grabber.cpp
@@ -35,3 +35,67 @@ pcl::EnsensoGrabber::EnsensoGrabber () :
   {
     nxLibInitialize ();
     root_.reset (new NxLibItem);
+  }
+  catch (NxLibException &ex)
+  {
+    ensensoExceptionHandling (ex, "EnsensoGrabber");
+    PCL_THROW_EXCEPTION (pcl::IOException, "Could not initialise NxLib.");  // If constructor fails; throw exception
+  }
+}
+
+pcl::EnsensoGrabber::~EnsensoGrabber () throw ()
+{
+  try
+  {
+    stop ();
+    root_.reset ();
+
+    disconnect_all_slots<sig_cb_ensenso_point_cloud> ();
+    disconnect_all_slots<sig_cb_ensenso_point_cloud_rgb> ();
+    disconnect_all_slots<sig_cb_ensenso_images> ();
+    disconnect_all_slots<sig_cb_ensenso_images_rgb> ();
+    disconnect_all_slots<sig_cb_ensenso_image_depth> ();
+
+    if (tcp_open_)
+      closeTcpPort ();
+    nxLibFinalize ();
+  }
+  catch (...)
+  {
+    // destructor never throws
+  }
+}
+
+bool pcl::EnsensoGrabber::calibrateHandEye (const std::vector<Eigen::Affine3d, Eigen::aligned_allocator<Eigen::Affine3d> > &robot_poses,
+                                            const Eigen::Affine3d &camera_seed,
+                                            const Eigen::Affine3d &pattern_seed,
+                                            const std::string setup,
+                                            Eigen::Affine3d &estimated_camera_pose,
+                                            Eigen::Affine3d &estimated_pattern_pose,
+                                            int &iterations,
+                                            double &reprojection_error) const
+{
+  if ( (*root_)[itmVersion][itmMajor] <= 1 && (*root_)[itmVersion][itmMinor] < 3)
+  {
+    PCL_WARN("EnsensoSDK 1.3.x fixes bugs into calibration optimization\n");
+    PCL_WARN("please update your SDK! http://www.ensenso.de/support/sdk-download\n");
+  }
+  NxLibCommand calibrate (cmdCalibrateHandEye);
+  try
+  {
+    // Check consistency
+    if (!boost::iequals (setup, valFixed) && !boost::iequals (setup, valMoving))
+    {
+      PCL_WARN("Received invalid setup value: %s\n", setup.c_str());
+      return (false);
+    }
+    // Set Hand-Eye calibration parameters
+    PCL_DEBUG("Setting calibration parameters\n");
+    std::string target;
+    if (boost::iequals (setup, valFixed))
+      target = valWorkspace;
+    else
+      target = valHand;
+    Eigen::Affine3d eigen_pose;
+    // Feed robot transformations
+    PCL_DEBUG("Populating robot poses\n");
